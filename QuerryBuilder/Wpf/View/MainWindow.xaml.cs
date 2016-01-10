7ddb6fa0e998c0a9e4fcb4558f4e2ace892032fb
unknown <dmitriy.fokasii@gmail.com>
2016-01-11 00:18:26 +0200

    clear

--- QuerryBuilder/Wpf/View/MainWindow.xaml.cs
+++ QuerryBuilder/Wpf/View/MainWindow.xaml.cs
@@ -15,16 +15,6 @@ namespace Wpf
         public MainWindow()
         {
             InitializeComponent();
-
-            EntityManager manager = new EntityManager();
-            Users user=manager.RegistrationUser("Dmitriy", "Fokasii", "fokas.dima@yandex.ru", "12345");
-
-            Debug.WriteLine(user.FirstName);
-
-            
-
-
-
             //var ff = new AutorizationForm();
             //var ss = new RegistrationForm();
             //var dd = new ConnectionDBForm();

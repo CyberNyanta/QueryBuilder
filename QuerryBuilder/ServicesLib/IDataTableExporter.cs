a2756786c50bd896e8b000b05b365dbb613e93fa
unknown <valeriy.starik@meta.ua>
2016-01-14 09:39:10 +0200

    merge

---
+++ QuerryBuilder/ServicesLib/IDataTableExporter.cs
@@ -0,0 +1,11 @@
+﻿using System.Data;
+
+namespace ServicesLib
+{
+    public interface IDataTableExporter
+    {
+        bool IncludeTitle { get; set; }
+
+        void DataTableExport(DataTable dataTable, string filePath, string title);
+    }
+}

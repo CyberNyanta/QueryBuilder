33e03d72914144430607639befc5bf6e0726e58e
unknown <valeriy.starik@meta.ua>
2016-01-19 16:41:17 +0200

    Add from send email, minor fix

---
+++ QuerryBuilder/Wpf/ViewModel/EmailViewModel.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Wpf.DataModel;
+
+namespace Wpf.ViewModel
+{
+    class EmailViewModel : IDataErrorInfo
+    {
+        public string Email { get; set; }
+        public string Title { get; set; }
+        public string SqlQuerry { get; set; }
+
+        public ICommand ClickSendMailCommand { get; set; }
+        public Action CloseAction { get; set; }
+
+        public EmailViewModel()
+        {
+            ClickSendMailCommand = new RelayCommand(arg => ClickMethodSendEmail());
+        }
+
+        private void ClickMethodSendEmail()
+        {
+            try
+            {
+                var mail = ServicesLib.SmtpMailer.Instance();
+                mail.SendMail(Email, Title, SqlQuerry);
+                this.CloseAction();
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBox.Show("Mail sent", "", button, icon);
+                }
+            catch (Exception)
+            {
+
+                MessageBoxImage icon = MessageBoxImage.Error;
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBox.Show("Mail didn't send", "", button, icon);
+            }
+                        
+        }
+
+        public string this[string columnName]
+        {
+
+            get
+            {
+
+                var error = string.Empty;
+                switch (columnName)
+                {
+                    case "Email":
+                        if (Email == null)
+                        {
+                            error = "Enter your e-mail";
+                        }
+                        else if (!ValidationMethods.EmailValidation(Email))
+                        {
+                            error = "Enter correct e-mail";
+                        }
+
+                        break;
+                   }
+                return error;
+            }
+        }
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
+
--- QuerryBuilder/Wpf/ViewModel/MainWindowFormViewModel.cs
+++ QuerryBuilder/Wpf/ViewModel/MainWindowFormViewModel.cs
@@ -2,20 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
 using Wpf.View;
 
 namespace Wpf.ViewModel
 {
-    class MainWindowFormViewModel:Notifier
+   class MainWindowFormViewModel:Notifier
     {
+        
         private static ObservableCollection<Group> _list;
 
         public ICommand ClickAutorizationCommand { get; set; }
         public ICommand ClickAddConnectionCommand { get; set; }
         public ICommand ClickCloseCommand { get; set; }
         public ICommand ClickNewProjectCommand { get; set; }
+        public ICommand ClickSaveExcelCommand { get; set; }
+        public ICommand ClickSaveTxtCommand { get; set; }
+        public ICommand ClickSavePdfCommand { get; set; }
+        public ICommand ClickSendQuerryToEmailCommand { get; set; }
+
         public string SqlQuerry { get; set; }
 
         public MainWindowFormViewModel()
@@ -23,7 +31,82 @@ namespace Wpf.ViewModel
             ClickAutorizationCommand = new RelayCommand(arg => ClickMethodAutorization());
             ClickAddConnectionCommand = new RelayCommand(arg => ClickMethodAddConection());
             ClickNewProjectCommand = new RelayCommand(arg => ClickMethodAddProject());
+            ClickSaveExcelCommand = new RelayCommand(arg => ClickMethodSaveExcel());
+            ClickSaveTxtCommand = new RelayCommand(arg => ClickMethodSaveTxt());
+            ClickSavePdfCommand = new RelayCommand(arg => ClickMethodSavePdf());
+            ClickSendQuerryToEmailCommand = new RelayCommand(arg => ClickMethodSendQuerryToEmail());
             _list = new ObservableCollection<Group>();
+        }
+
+        private void ClickMethodSendQuerryToEmail()
+        {
+           var SendMailWindow = new Email();
+           SendMailWindow.ShowDialog();
+
+        }
+       
+        /// <summary>
+        /// Метод команды, сохраняющий запрос в TXT
+        /// </summary>
+        private void ClickMethodSaveTxt()
+        {
+            SaveTXT(SqlQuerry);
+        }
+
+        public static void SaveTXT(string querry)
+        {
+            // Configure save file dialog box
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "ResultQuerry"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents |*.txt"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+                using (var sw = new StreamWriter(filename))
+                {
+                    sw.WriteLine(querry);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод команды, сохраняющий результат запроса в PDF
+        /// </summary>
+        private void ClickMethodSavePdf()
+        {
+            DataTable cc = new DataTable();
+            cc.Columns.Add("Column1");
+            cc.Rows.Add("1");
+            cc.Rows.Add("2");
+            SavePDF(cc, "title");
+        }
+
+        public static void SavePDF(DataTable table, string title)
+        {
+            // Configure save file dialog box
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "ResultQuerry"; // Default file name
+            dlg.DefaultExt = ".pdf"; // Default file extension
+            dlg.Filter = "Text documents |*.pdf"; // Filter files by extension
+
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+                var file = ServicesLib.DataTableToPdfExporter.CreateInstance();
+                file.DataTableExport(table, filename, title);
+            }
         }
 
         private void ClickMethodAddProject()
@@ -50,7 +133,40 @@ namespace Wpf.ViewModel
             var windowConnectionDbForm = new ConnectionDbForm();
             windowConnectionDbForm.Show();
         }
+        /// <summary>
+        /// Метод команды, сохраняющий результат запроса в Эксель
+        /// </summary>
+        private void ClickMethodSaveExcel()
+        {
+            DataTable table = null;
+            SaveExcel(table, "title");
+        }
+        /// <summary>
+        /// Метод сохранения в Эксель. Принимает таблицу и заголовок.
+        /// Вызывает диалоговое окно сохранения в файл
+        /// </summary>
+        /// <param name="table"></param>
+        /// <param name="Title"></param>
+        public static void SaveExcel(DataTable table, string Title)
+        {
+            // Configure save file dialog box
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.FileName = "ResultQuerry"; // Default file name
+            dlg.DefaultExt = ".xlsx"; // Default file extension
+            dlg.Filter = "Text documents |*.xlsx"; // Filter files by extension
 
+            // Show save file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process save file dialog box results
+            if (result == true)
+            {
+                // Save document
+                string filename = dlg.FileName;
+                var file = ServicesLib.DataTableToExcelExporter.CreateInstance();
+                file.DataTableExport(table, filename, Title);
+            }
+        }
         public ObservableCollection<Group> List
         {
             get { return _list; }

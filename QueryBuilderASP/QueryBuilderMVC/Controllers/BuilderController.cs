7511f14840270e6bc6353074d313c8e806dbe1bb
eugenesianko <eugenesianko@gmail.com>
2016-03-30 17:13:18 +0300

    Add GoJS library

---
+++ QueryBuilderASP/QueryBuilderMVC/Controllers/BuilderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QueryBuilderMVC.Controllers
+{
+    public class BuilderController : Controller
+    {
+        // GET: Builder
+        public ActionResult ERModel()
+        {
+            return View();
+        }
+    }
+}

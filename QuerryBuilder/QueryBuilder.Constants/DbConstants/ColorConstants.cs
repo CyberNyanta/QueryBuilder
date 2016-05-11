5e0eb5b2b5c599d3d276eb277bb6c2a990c549f2
Valera starik <valeriy.starik@meta.ua>
2016-05-11 14:33:23 +0300

    DON'T WORK

---
+++ QuerryBuilder/QueryBuilder.Constants/DbConstants/ColorConstants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryBuilder.Constants.DbConstants
+{
+    public abstract class ColorConstants
+    {
+        public const string Blue = "#2e83ff";
+        public const string Cyan = "#46b8da";
+        public const string Gray = "#c0c0c0";
+    }
+}

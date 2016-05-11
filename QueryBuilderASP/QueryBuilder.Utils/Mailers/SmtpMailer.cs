1f85bd53b9866ed10821d8187755e14f686c2949
anton.datskovskiy <anton.datskovskiy@gmail.com>
2016-05-12 01:43:35 +0300

    QBM2T2-147. Sending the result by e-mail.

--- QueryBuilderASP/QueryBuilder.Utils/Mailers/SmtpMailer.cs
+++ QueryBuilderASP/QueryBuilder.Utils/Mailers/SmtpMailer.cs
@@ -60,14 +60,12 @@ namespace QueryBuilder.Utils.Mailers
                             {
                                 foreach (var filePath in filesPathes)
                                 {
-                                    if (File.Exists(filePath))
-                                    {
-                                        // Create  the file attachment for this e-mail message.
-                                        var data = new Attachment(filePath, MediaTypeNames.Application.Octet);
+                                    if (!File.Exists(filePath)) continue;
+                                    // Create  the file attachment for this e-mail message.
+                                    var data = new Attachment(filePath, MediaTypeNames.Application.Octet);
 
-                                        // Add the file attachment to this e-mail message.
-                                        message.Attachments.Add(data);
-                                    }
+                                    // Add the file attachment to this e-mail message.
+                                    message.Attachments.Add(data);
                                 }
                             }
 
@@ -90,9 +88,70 @@ namespace QueryBuilder.Utils.Mailers
             return flagSend;
         }
 
+        /// <summary>
+        /// Method for send mail
+        /// Multiple e-mail addresses must be separated with a comma character (",").
+        /// Mail settings get from app.config
+        /// </summary>
+        /// <param name="addressesTo"></param>
+        /// <param name="subjectMail"></param>
+        /// <param name="bodyMail"></param>
+        /// <param name="stream">Memory stream for attach</param>
+        /// <returns></returns>
+        public bool SendMail(string addressesTo, string subjectMail, string bodyMail, MemoryStream stream)
+        {
+            bool flagSend;
+
+            if (string.IsNullOrWhiteSpace(addressesTo))
+            {
+                throw new ArgumentException("Incorrect e-mail address (To). ");
+            }
+
+            // Verify mail settings
+            if (_mailSettings != null)
+            {
+                using (var mailClient = new SmtpClient())
+                {
+                    using (var message = new MailMessage())
+                    {
+                        try
+                        {
+                            message.From = new MailAddress(_mailSettings.Smtp.Network.UserName, "AltexSoftLab");
+                            message.To.Add(addressesTo);
+                            message.Subject = subjectMail;
+                            message.Body = bodyMail;
+
+                            var contentType = new ContentType(MediaTypeNames.Application.Octet);
+                            var attach = new Attachment(stream, contentType);
+                            attach.ContentDisposition.FileName = "Result.xlsx";                            
+
+                            // Add the file attachment to this e-mail message.
+                            message.Attachments.Add(attach);
+
+                            mailClient.Send(message);
+
+                            stream.Close();
+
+                            flagSend = true;
+                        }
+                        catch (SmtpException e)
+                        {
+                            throw new SmtpException("Mail.Send: " + e.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("Empty mail settings.");
+            }
+
+            return flagSend;
+        }
+
         public bool SendRegisterNotification(string email)
         {
-            return SendMail(email, "AltexSoft M2T2", "Thank you for registering!", null);
+            return SendMail(email, "AltexSoft M2T2", "Thank you for registering!");
         }
 
         public Task SendMailAsync(string addressesTo, string subjectMail, string bodyMail, string[] filesPathes = null)
@@ -118,14 +177,12 @@ namespace QueryBuilder.Utils.Mailers
                             {
                                 foreach (var filePath in filesPathes)
                                 {
-                                    if (File.Exists(filePath))
-                                    {
-                                        // Create  the file attachment for this e-mail message.
-                                        var data = new Attachment(filePath, MediaTypeNames.Application.Octet);
+                                    if (!File.Exists(filePath)) continue;
+                                    // Create  the file attachment for this e-mail message.
+                                    var data = new Attachment(filePath, MediaTypeNames.Application.Octet);
 
-                                        // Add the file attachment to this e-mail message.
-                                        message.Attachments.Add(data);
-                                    }
+                                    // Add the file attachment to this e-mail message.
+                                    message.Attachments.Add(data);
                                 }
                             }
 
@@ -147,7 +204,7 @@ namespace QueryBuilder.Utils.Mailers
 
         public Task SendAsyncRegisterNotification(string email)
         {
-            return SendMailAsync(email, "AltexSoft M2T2", "Thank you for registering!", null);
+            return SendMailAsync(email, "AltexSoft M2T2", "Thank you for registering!");
         }
 
     }

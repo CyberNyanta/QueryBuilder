06eca37e8a022da7152b9d08521944188e781023
Valera starik <valeriy.starik@meta.ua>
2016-05-23 18:07:46 +0300

    don't work TEST

--- QueryBuilderASP/QueryBuilderMVC/Controllers/AccountController.cs
+++ QueryBuilderASP/QueryBuilderMVC/Controllers/AccountController.cs
@@ -60,7 +60,7 @@ namespace QueryBuilderMVC.Controllers
         public ActionResult Login(string returnUrl)
         {
             ViewBag.ReturnUrl = returnUrl;
-            return View();
+            return PartialView("Login");
         }
 
         //

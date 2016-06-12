de2bb1da8039d02802f05593f62d08542b16dce0
eugenesianko <eugenesianko@gmail.com>
2016-06-12 20:49:50 +0300

    add relations to xml parcer

--- QueryBuilderASP/QueryBuilder.Utils/DBSchema/CustomMetadataProvider.cs
+++ QueryBuilderASP/QueryBuilder.Utils/DBSchema/CustomMetadataProvider.cs
@@ -21,12 +21,9 @@ namespace QueryBuilder.Utils.DBSchema
 		const string TABLE_SCHEMA = "TABLE_SCHEMA";
 		const string PROCEDURE_PARAMETERS = "PROCEDURE_PARAMETERS";
 
-		const string querry = "Select kcup.Table_name, kcuc.TABLE_NAME " +
-								  "From INFORMATION_SCHEMA.REFERENTIAL_CONSTRAINTS rc " +
-								  "Left Join INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcup On " +
-								  "rc.UNIQUE_CONSTRAINT_NAME = kcup.CONSTRAINT_NAME " +
-								  "LEFT Join INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcuc on " +
-								  "rc.CONSTRAINT_NAME = kcuc.CONSTRAINT_NAME and kcup.ORDINAL_POSITION = kcuc.ORDINAL_POSITION";
+		//SELECT f.name AS ForeignKey, OBJECT_NAME(f.parent_object_id) AS TableName, COL_NAME(fc.parent_object_id, fc.parent_column_id) AS ColumnName, OBJECT_NAME (f.referenced_object_id) AS ReferenceTableName, COL_NAME(fc.referenced_object_id, fc.referenced_column_id) AS ReferenceColumnName FROM sys.foreign_keys AS f INNER JOIN sys.foreign_key_columns AS fc ON f.OBJECT_ID = fc.constraint_object_id
+		const string querry = "SELECT OBJECT_NAME(f.parent_object_id) AS TableName, COL_NAME(fc.parent_object_id, fc.parent_column_id) AS ColumnName, OBJECT_NAME (f.referenced_object_id) AS ReferenceTableName, COL_NAME(fc.referenced_object_id, fc.referenced_column_id) AS ReferenceColumnName FROM sys.foreign_keys AS f INNER JOIN sys.foreign_key_columns AS fc ON f.OBJECT_ID = fc.constraint_object_id";
+		const string querryPK = "select TABLE_NAME,COLUMN_NAME from INFORMATION_SCHEMA.KEY_COLUMN_USAGE where CONSTRAINT_NAME like 'PK%'";
 
 		static Dictionary<string, string[]> datatypesConditions = new Dictionary<string, string[]>
 		{
@@ -82,42 +79,82 @@ namespace QueryBuilder.Utils.DBSchema
 			{
 				var databaseElement = new XElement("database", new XAttribute("name", connection.Database), new XAttribute("default", "True"));
 				metadata.Add(databaseElement);
+				var shemaElement = new XElement("schema", new XAttribute("name", "dbo"), new XAttribute("default", "True"));
+				databaseElement.Add(shemaElement);
+
+				var nodesTable = new DataTable();
+				var keyTable = new DataTable();
+				var command = new SqlCommand(querry, connection);
+				var keyCommand = new SqlCommand(querryPK, connection);
 
 				connection.Open();
 				var dataTables = connection.GetSchema("Tables", new string[4] { null, null, null, TABLE});
 				var tablesColumns = connection.GetSchema("Columns");
-				//var keys = connection.GetSchema("KEY_COLUMN_USAGE");
+				using (SqlDataReader dr = command.ExecuteReader())
+				{
+					nodesTable.Load(dr);
+				}
+				using (SqlDataReader dr = keyCommand.ExecuteReader())
+				{
+					keyTable.Load(dr);
+				}
+
 				connection.Close();
 
-				foreach(DataRow dr in dataTables.Rows)
+
+				foreach (DataRow dr in dataTables.Rows)
 				{
 					var tableName = (string)dr[TABLE_NAME];
 					var tableElement = new XElement("table", new XAttribute("name", tableName));
-					databaseElement.Add(tableElement);
-					
+					shemaElement.Add(tableElement);
+					var keys = keyTable.AsEnumerable().Where(row => row.Field<string>(TABLE_NAME).Equals(tableName));
 
 					foreach (DataRow drColumns in tablesColumns.AsEnumerable().Where(row => row.Field<string>(TABLE_NAME).Equals(tableName)))
 					{
 						var datatype = drColumns["DATA_TYPE"];
-						var name = new XAttribute("name", drColumns["COLUMN_NAME"]);
+						var columnName = drColumns["COLUMN_NAME"].ToString();
+						var name = new XAttribute("name", columnName);
 						var type_name = new XAttribute("type_name", datatype);
-						var type= new XAttribute("type", datatypesConditions[(string)datatype][0]);
+						var type = new XAttribute("type", datatypesConditions[(string)datatype][0]);
 						var sizetemp = drColumns["CHARACTER_MAXIMUM_LENGTH"].ToString();
-						var size = new XAttribute("size",String.IsNullOrWhiteSpace(sizetemp)? datatypesConditions[(string)datatype][1]: sizetemp);
+						var size = new XAttribute("size", String.IsNullOrWhiteSpace(sizetemp) ? datatypesConditions[(string)datatype][1] : sizetemp);
 						var precision = new XAttribute("precision", datatypesConditions[(string)datatype][2]);
 						var scale = new XAttribute("scale", datatypesConditions[(string)datatype][3]);
-						var nullable = new XAttribute("nullable", ((string)drColumns["IS_NULLABLE"]).Equals("YES")? "True" : "False");
-						tableElement.Add(new XElement("field", name, type_name, type, size, precision, scale, nullable));
+						var nullable = new XAttribute("nullable", ((string)drColumns["IS_NULLABLE"]).Equals("YES") ? "True" : "False");
+						var column = new XElement("field", name, type_name, type, size, precision, scale, nullable);
+						foreach(DataRow drKeys in keys)
+						{
+							if (columnName.Equals(drKeys["COLUMN_NAME"]))
+							{
+								column.Add(new XAttribute("primary_key", "True"));
+							}
+						}
+
+						tableElement.Add(column);
 
 					}
 
+					foreach (DataRow drColumns in nodesTable.AsEnumerable().Where(row => row.Field<string>("TableName").Equals(tableName)))
+					{
+						tableElement.Add(new XElement("foreignkey",
+							new XElement("referenced_object",
+								new XElement("name", drColumns["ReferenceTableName"].ToString()),
+								new XElement("name", "dbo"),
+								new XElement("name", connection.Database)),
+							new XElement("referencing_field",
+								new XAttribute("name", drColumns["ColumnName"].ToString()),
+								new XAttribute("referenced_field", drColumns["ReferenceColumnName"].ToString())),
+							new XElement("referencing_cardinality", "Many"),
+							new XElement("referenced_cardinality", "One")
+								));
+					}
 				}
 
 			}
 			
 			var Result = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" + metadata.ToString(SaveOptions.DisableFormatting);
 			return Result;
-			//return "<?xml version=\"1.0\" encoding=\"utf-8\"?><metadata xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://www.activequerybuilder.com/schemas/metadata2.xsd\">	<database name=\"Northwind\" default=\"True\">		<table name=\"Categories\">	<field name=\"CategoryID\" type_name=\"int\" type=\"Int32\" size=\"8\" precision=\"10\" scale=\"255\" nullable=\"False\" readonly=\"True\" />	<field name=\"CategoryName\" type_name=\"nvarchar\" type=\"String\" size=\"15\" precision=\"255\" scale=\"255\" nullable=\"False\" />	<field name=\"Description\" type_name=\"ntext\" type=\"String\" size=\"1073741823\" precision=\"255\" scale=\"255\" />	<field name=\"Picture\" type_name=\"image\" type=\"Binary\" size=\"2147483647\" precision=\"255\" scale=\"255\" /></table></database></metadata>";
+			
 		}
 
 		private static string GetSelectStatement(DataTable table)
